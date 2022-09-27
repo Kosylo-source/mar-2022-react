bcb7db6a4d27c2a0c4e5ab2d6c5d2d6824c6f5c4
kpsylo-react <volodiakosylo@gmail.com>
2022-09-27 21:29:53 +0300

    create branch HW-1 Simpsons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import Characters from "./components/Characters";
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+<Characters
+    name={'Bart'}
+    surname={'Simpson'}
+    age={10}
+    info={'Бартоломю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі'}
+    photo={'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'}
+    alt={'Bart'}
+/>
+          <Characters
+    name={'Homer'}
+    surname={'Simpson'}
+    age={40}
+    info={'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий..'}
+    photo={'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}
+    alt={'Homer'}
+/>
+      </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Characters.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Characters(props) {
+    const { name, surname, age, info, photo, alt } = props;
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>{surname}</p>
+            <div>{age}</div>
+            <div>{info}</div>
+            <img src={photo} alt={alt}></img>
+        </div>
+    );
+}
+
+export default Characters;
